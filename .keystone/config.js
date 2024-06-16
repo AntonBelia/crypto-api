0e922d9db5f8def749b48da9fcc880eba64b3b8c
Belia Anton <belja.anton@gmail.com>
2024-06-16 12:43:08 +0300

    delete post and tag

--- .keystone/config.js
+++ .keystone/config.js
@@ -30,7 +30,6 @@ var import_core = require("@keystone-6/core");
 var import_access = require("@keystone-6/core/access");
 var import_fields = require("@keystone-6/core/fields");
 var import_fields2 = require("@keystone-6/core/fields");
-var import_fields_document = require("@keystone-6/fields-document");
 var lists = {
   Player: (0, import_core.list)({
     access: import_access.allowAll,
@@ -49,61 +48,9 @@ var lists = {
         isIndexed: "unique"
       }),
       password: (0, import_fields2.password)({ validation: { isRequired: true } }),
-      posts: (0, import_fields2.relationship)({ ref: "Post.author", many: true }),
       createdAt: (0, import_fields2.timestamp)({
         defaultValue: { kind: "now" }
       })
-    }
-  }),
-  Post: (0, import_core.list)({
-    access: import_access.allowAll,
-    fields: {
-      title: (0, import_fields2.text)({ validation: { isRequired: true } }),
-      content: (0, import_fields_document.document)({
-        formatting: true,
-        layouts: [
-          [1, 1],
-          [1, 1, 1],
-          [2, 1],
-          [1, 2],
-          [1, 2, 1]
-        ],
-        links: true,
-        dividers: true
-      }),
-      author: (0, import_fields2.relationship)({
-        ref: "User.posts",
-        ui: {
-          displayMode: "cards",
-          cardFields: ["name", "email"],
-          inlineEdit: { fields: ["name", "email"] },
-          linkToItem: true,
-          inlineConnect: true
-        },
-        many: false
-      }),
-      tags: (0, import_fields2.relationship)({
-        ref: "Tag.posts",
-        many: true,
-        ui: {
-          displayMode: "cards",
-          cardFields: ["name"],
-          inlineEdit: { fields: ["name"] },
-          linkToItem: true,
-          inlineConnect: true,
-          inlineCreate: { fields: ["name"] }
-        }
-      })
-    }
-  }),
-  Tag: (0, import_core.list)({
-    access: import_access.allowAll,
-    ui: {
-      isHidden: true
-    },
-    fields: {
-      name: (0, import_fields2.text)(),
-      posts: (0, import_fields2.relationship)({ ref: "Post.tags", many: true })
     }
   })
 };
@@ -119,20 +66,10 @@ if (!sessionSecret && process.env.NODE_ENV !== "production") {
 var { withAuth } = (0, import_auth.createAuth)({
   listKey: "User",
   identityField: "email",
-  // this is a GraphQL query fragment for fetching what data will be attached to a context.session
-  //   this can be helpful for when you are writing your access control functions
-  //   you can find out more at https://keystonejs.com/docs/guides/auth-and-access-control
   sessionData: "name createdAt",
   secretField: "password",
-  // WARNING: remove initFirstItem functionality in production
-  //   see https://keystonejs.com/docs/config/auth#init-first-item for more
   initFirstItem: {
-    // if there are no items in the database, by configuring this field
-    //   you are asking the Keystone AdminUI to create a new user
-    //   providing inputs for these fields
     fields: ["name", "email", "password"]
-    // it uses context.sudo() to do this, which bypasses any access control you might have
-    //   you shouldn't use this in production
   }
 });
 var sessionMaxAge = 60 * 60 * 24 * 30;
